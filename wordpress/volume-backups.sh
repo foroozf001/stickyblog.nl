a57c747911d7224866f228e4d0891f7416f9e846
foroozf001 <farazforoozan@gmail.com>
2021-07-24 22:56:25 +0200

    Corrected volume-backups script tarball command

--- wordpress/volume-backups.sh
+++ wordpress/volume-backups.sh
@@ -2,6 +2,9 @@
 ssh -i ~/Certs/private.pem ubuntu@ec2-108-128-217-232.eu-west-1.compute.amazonaws.com "sudo cp -r /var/lib/docker/volumes/ /home/ubuntu/bak/;sudo chmod -R +rwx /home/ubuntu/bak/;"
 
 scp -i ~/Certs/private.pem -r ubuntu@ec2-108-128-217-232.eu-west-1.compute.amazonaws.com:/home/ubuntu/bak/ /home/foroozf001/Backups/
-tar -czvf stickyblog.bak.tar.gz ~/Backups/
+
+tar -czvf ~/Backups/stickyblog.bak.tar.gz ~/Backups/
+
+rm -r ~/Backups/bak/
 
 ssh -i ~/Certs/private.pem ubuntu@ec2-108-128-217-232.eu-west-1.compute.amazonaws.com "sudo rm -r bak/;"
