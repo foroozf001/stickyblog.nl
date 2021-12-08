bc894975aff0831b741b06b38562f04b20148f4c
fforoozan <faraz.foroozan@vodafoneziggo.com>
2021-12-08 16:10:23 +0100

    Test

--- terraform/modules/ec2/elastic-ip.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_eip" "web" {
-  instance = aws_instance.web.id
-  vpc      = true
-}
--- terraform/production/main.tf
+++
@@ -1,7 +0,0 @@
-module "main" {
-  source            = "../modules/ec2"
-  ami = var.ami
-  ec2_instance_type = var.ec2_instance_type
-  allowed_ips       = var.allowed_ips
-  tags              = var.tags
-} 
